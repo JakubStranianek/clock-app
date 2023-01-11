e17d97113c5f54e1ac6dd32f822d00f84999efe1
Jakub Stranianek <j.stranianek@gmail.com>
2023-01-11 13:06:04 +0100

    mobile problem fix 1.0

--- clock-app/src/App.js
+++ clock-app/src/App.js
@@ -8,51 +8,60 @@ import { useEffect, useState } from "react";
 import Popup from "./components/Popup";
 
 function App() {
-  const date = new Date()
-  const [hour] = useState(date.getHours())
+  const date = new Date();
+  const [hour] = useState(date.getHours());
   const [main, setMain] = useState(true);
   const [timezone, setTimezone] = useState();
 
   useEffect(() => {
-    setMain(!main)    
-  }, [])
+    setMain(!main);
+  }, []);
 
   return (
-    <div className={hour <= 17 ? "bg-dayMobile bg-no-repeat bg-cover bg-center bg-blend-lighten h-screen md:bg-dayTablet lg:bg-dayDesktop" : 
-    "bg-nightMobile bg-no-repeat bg-cover bg-center bg-blend-lighten h-screen md:bg-nightTablet lg:bg-nightDesktop"}>
-
+    <div
+      className={
+        hour <= 17
+          ? "bg-dayMobile bg-no-repeat bg-cover bg-center bg-blend-lighten h-screen md:bg-dayTablet lg:bg-dayDesktop"
+          : "bg-nightMobile bg-no-repeat bg-cover bg-center bg-blend-lighten h-screen md:bg-nightTablet lg:bg-nightDesktop"
+      }
+    >
       <div className="h-full w-11/12 mx-auto md:w-10/12 lg:w-3/4">
         <div className="relative z-50 h-full lg:w-full flex flex-col justify-between">
-        {main ?  
-         ""
-        : 
-        <header className="lg:w-2/3">
-        <HeaderCaption />
-      </header> }
+          {main ? (
+            ""
+          ) : (
+            <header className="lg:w-2/3">
+              <HeaderCaption />
+            </header>
+          )}
 
-          <main className={main ? "w-full absolute top-[15%] md:top-[5%] lg:top-[10%]" : ""}>
+          <main
+            className={
+              main ? "w-full absolute top-[15%] md:top-[5%] lg:top-[10%]" : ""
+            }
+          >
             <div className="lg:pb-24 lg:flex lg:items-end lg:justify-between">
-              <div>
-                <Welcome setWelcome={hour} />
+              <div className="lg:w-full lg:flex lg:justify-between lg:items-end">
+                <div>
+                  <Welcome setWelcome={hour} />
 
-                <div className="py-4">
-                  <Time />            
+                  <div className="py-4">
+                    <Time />
+                  </div>
+
+                  <Location setTimezone={setTimezone} />
                 </div>
 
-                <Location setTimezone={setTimezone} />
-              </div>
-
-              <div className="pt-12 pb-10 md:pt-20 md:pb-16 lg:py-0">
-                <Button setMain={setMain}/>                           
+                <div className="pt-12 pb-10 md:pt-20 md:pb-16 lg:py-0">
+                  <Button setMain={setMain} />
+                </div>
               </div>
             </div>
           </main>
         </div>
       </div>
 
-      {main ? 
-      <Popup setPopup={hour} sentTimezone={timezone} /> 
-      : ""}
+      {main ? <Popup setPopup={hour} sentTimezone={timezone} /> : ""}
     </div>
   );
 }
